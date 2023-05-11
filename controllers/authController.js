a4afd6b86602187d86236a99c7d67e440a10268a
Phil <phillippelliott@gmail.com>
2023-05-11 16:21:33 +0700

    added signup and login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,57 @@
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async (req, res) => {};
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
-export const login = async (req, res) => {};
+export const signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    console.log(newUser);
+
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+      status: "success",
+      token,
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1) Check if email and password exist
+    if (!email || !password) {
+      return next(new AppError("Please provide email and password!", 400));
+    }
+
+    // 2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError("Incorrect email or password", 401));
+    }
+
+    // 3) If everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: "success",
+      token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const logout = async (req, res) => {};
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -31,6 +32,25 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
